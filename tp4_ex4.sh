76a9e5749d634cb6b4bae4bf23d7c794dac0cba6
R-Gld <romain.galland@rgld.r>
2025-02-19 00:58:56 +0100

    Removing extra spaces.

--- tp4_ex4.sh
+++ tp4_ex4.sh
@@ -1,8 +1,6 @@
-#!/bin/bash                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                   
- # Display multithreaded processes with their PID, command, and thread count                                                                                                                                                       
- ps -eLf | awk 'NR!=1 {print $2}' | sort | uniq -c | awk '$1>1 {print $2}' | while read pid; do                                                                                                                                    
-     cmd=$(ps -p $pid -o comm=)                                                                                                                                                                                                    
-     thread_count=$(ps -eLf | awk -v pid=$pid '$2==pid {print $2}' | wc -l)                                                                                                                                                        
-     echo "PID: $pid CMD: $cmd Threads: $thread_count"                                                                                                                                                                             
- done
+#!/bin/bash
+ps -eLf | awk 'NR!=1 {print $2}' | sort | uniq -c | awk '$1>1 {print $2}' | while read pid; do
+    cmd=$(ps -p $pid -o comm=)
+    thread_count=$(ps -eLf | awk -v pid=$pid '$2==pid {print $2}' | wc -l)
+    echo "PID: $pid CMD: $cmd Threads: $thread_count"
+done
